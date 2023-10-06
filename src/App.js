4cad751ac98db88a840562150955f83a4093e4b3
saurabhgdeshmukh <saurabhgdeshmukh@gmail.com>
2023-10-07 05:13:18 +0530

    Spinner and catagories added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 
 import './App.css';
+
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
+
 export default class App extends Component {
+
   render() {
     return (
-      <div><NavBar/>
-      <News/></div>
-      
+      <div>
+        <NavBar/>
+        <News pageSize={5} country="in" category="science"/>
+      </div>
     )
   }
 }
